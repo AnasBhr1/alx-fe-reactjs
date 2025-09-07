5d05e7c6e98cf5cf76c1cd94d0a4ef9257511d56
Anasbhr1 <anasbhr1@hotmail.com>
2025-09-07 17:29:22 +0100

    fix

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Contact from "./components/Contact";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
